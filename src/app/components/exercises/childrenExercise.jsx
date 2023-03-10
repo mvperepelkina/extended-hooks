fa4a4150345f7cbf0bc4302a5c0f230d101b18f3
mvperepelkina <marvladperepelkina@gmail.com>
2023-03-10 23:27:18 +0300

    feat: added task solution

---
+++ src/app/components/exercises/childrenExercise.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import CollapseWrapper from "../common/collapse";
+import PropTypes from "prop-types";
+
+const ListComponent = ({ children }) => {
+    return React.Children.map(children, (child, number) => {
+        return React.cloneElement(child, { number: number + 1 });
+    });
+};
+
+ListComponent.propTypes = {
+    children: PropTypes.oneOfType([
+        PropTypes.arrayOf(PropTypes.node),
+        PropTypes.node
+    ])
+};
+
+const ChildrenExercise = () => {
+    return (
+        <CollapseWrapper title="Упражнение">
+            <p className="mt-3">
+                У вас есть компоненты Списка. Вам необходимо к каждому из них
+                добавить порядковый номер, относительно того, как они
+                располагаются на странице. Вы можете использовать как{" "}
+                <code>React.Children.map</code> так и{" "}
+                <code>React.Children.toArray</code>
+            </p>
+            <ListComponent>
+                <Component />
+                <Component />
+                <Component />
+            </ListComponent>
+        </CollapseWrapper>
+    );
+};
+
+const Component = ({ number }) => {
+    return <div>{number}. Компонент списка</div>;
+};
+
+Component.propTypes = { number: PropTypes.number };
+
+export default ChildrenExercise;
